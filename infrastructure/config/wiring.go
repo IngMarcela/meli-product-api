8d874c543f763019b63c5a82d1a4eb0eafb2da61
Marcela Domínguez <marceladominguez1993@gmail.com>
2025-06-16 23:15:49 -0500

    refactor: Simplify SetupApplication function by removing unnecessary comments and improving code clarity

--- infrastructure/config/wiring.go
+++ infrastructure/config/wiring.go
@@ -9,21 +9,17 @@ import (
 
 // SetupApplication configura todas las dependencias de la aplicación
 func SetupApplication(cfg *Config) (*fiber.App, error) {
-	// Crear instancia de Fiber
 	app := fiber.New(fiber.Config{
 		AppName: "Meli Product API",
 	})
 
-	// Inicializar repositorios
 	productRepo, err := repository.NewJSONProductRepository(cfg.ProductFile)
 	if err != nil {
 		return nil, err
 	}
 
-	// Inicializar queries
 	getByID := queries.NewGetProductByIDQuery(productRepo)
 
-	// Configurar rutas
 	RegisterRoutes(app, getByID, Logger)
 
 	return app, nil
